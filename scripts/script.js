704ca5e7489d017ec7c3eaf9a076d138a116e621
BOULARES-Hibs <hibablr.info@gmail.com>
2024-07-22 15:23:24 +0100

    media query

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,16 @@ const header =document.querySelector('.header');
 window.addEventListener('scroll',()=>{
    header.classList.toggle('scroll',scrollY>15);
 })
+
+const icon = document.querySelector('#menu-icon');
+const navlink = document.querySelector('.navlinks');
+icon.addEventListener('click', () => {
+      navlink.classList.toggle('opens');   
+      icon.classList.toggle('bx-x')
+      
+ });
+
+window.addEventListener('scroll',()=>{
+navlink.classList.remove('opens');
+icon.classList.remove('bx-x');
+})
